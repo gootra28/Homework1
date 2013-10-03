c1f2398c3d1794de30a58e50414fd71ad8516973
gootra28 <gootra28@evergreen.edu>
2013-10-03 14:31:32 -0700

    File

--- hw1.py
+++ hw1.py
@@ -1,5 +1,5 @@
 # Name: ...
-# Evergreen Login: ...
+# Evergreen Login: Gootra28
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 1
@@ -9,18 +9,21 @@
 # you are done.  When you are done, running this file should compute and
 # print the answers to all the problems.
 
-import math                     # makes the math.sqrt function available
-
+import math                     # makes the math.sqrt function availablea
 
 ###
 ### Problem 1
-###
+### 
 
 print "Problem 1 solution follows:"
+a = 1
+b = -5.86
+c = 8.5408
+Solution1 = (-b + math.sqrt(pow(b,2) - (4*a*c)))/(a*2)
+print Solution1
 
-# ... write your code and comments here (and remove this line)
-
-
+Solution2 = (-b - math.sqrt(pow(b,2) - (4*a*c)))/(a*2)
+print Solution2
 ###
 ### Problem 2
 ###
@@ -28,15 +31,20 @@ print "Problem 1 solution follows:"
 print "Problem 2 solution follows:"
 
 # ... write your code and comments here (and remove this line)
+import hw1_test
+
+print hw1_test.a
+print hw1_test.b
+print hw1_test.c
+print hw1_test.d
+print hw1_test.e
+print hw1_test.f
 
 
-###
-### Problem 3
-###
 
 print "Problem 3 solution follows:"
-
-# ... write your code and comments here (and remove this line)
+import hw1_test
+print ((hw1_test.a and hw1_test.b) or (not hw1_test.c) and not (hw1_test.d or hw1_test.e or hw1_test.f))
 
 
 ###
